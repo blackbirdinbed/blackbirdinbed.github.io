dce3a1b56aa2df36abbb6c8027ea9a4101a7ae9b
Iván Pérez Fernández <blackbirdinbed@gmail.com>
2020-11-13 20:52:41 +0100

    Changed who section

--- src/components/Who.vue
+++ src/components/Who.vue
@@ -4,12 +4,9 @@
       <b-col cols="12" md="5" class="mt-5">
         <p class="bold header-text">E.T.S.I.I.T. UGR</p>
         <p class="details-text">
-          Este proyecto está desarrollado por mí, Iván Pérez Fernández, alumno
-          del Grado en Ingeniería Informática por la Universidad de Granada.<br /><br />El
-          objetivo de esta web es dar a los alumnos un mecanismo para que
-          aprendan de manera más sencilla los contenidos de la asignatura
-          <span class="bold">Sistemas Concurrentes y Distribuidos</span> como
-          iniciativa del profesor de la Escuela José Miguel Mantas Ruiz.
+          Este proyecto está desarrollado por Iván Pérez Fernández, alumno
+          del Grado en Ingeniería Informática por la Universidad de Granada.<br /><br />En esta web podrás adquirir esos conocimientos que nos resultan más costosos. Esperamos que de esta manera algo que no nos atraía por la complejidad que tiene, nos termine gustando jugando con la simplicidad en los contenidos de la asignatura 
+          <span class="bold">Sistemas Concurrentes y Distribuidos</span> y así lograr las competencias exigidas para la misma.
         </p>
       </b-col>
       <b-col cols="12" md="7">
