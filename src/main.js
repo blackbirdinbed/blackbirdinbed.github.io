ad457f3600c55058a0becbb3508c461d92e694e1
Iván Pérez Fernández <blackbirdinbed@gmail.com>
2020-11-13 18:58:20 +0100

    Upload main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import "@lottiefiles/lottie-player";
+import "./assets/scss/main.scss";
 
-Vue.config.productionTip = false
+const base = axios.create({
+  baseURL: "https://web-animation-api.herokuapp.com"
+});
+
+library.add(fas);
+
+Vue.prototype.$http = base;
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
