04eb488062f15910d40e4b6e2f1c6c326b2efca2
Iván Pérez Fernández <blackbirdinbed@gmail.com>
2020-11-13 19:10:14 +0100

    Finished with who page

---
+++ src/components/Who.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-container class="home">
+    <b-row>
+      <b-col cols="12" md="5" class="mt-5">
+        <p class="bold header-text">E.T.S.I.I.T. UGR</p>
+        <p class="details-text">
+          Este proyecto está desarrollado por mí, Iván Pérez Fernández, alumno
+          del Grado en Ingeniería Informática por la Universidad de Granada.<br /><br />El
+          objetivo de esta web es dar a los alumnos un mecanismo para que
+          aprendan de manera más sencilla los contenidos de la asignatura
+          <span class="bold">Sistemas Concurrentes y Distribuidos</span> como
+          iniciativa del profesor de la Escuela José Miguel Mantas Ruiz.
+        </p>
+      </b-col>
+      <b-col cols="12" md="7">
+        <lottie-player
+          autoplay
+          loop
+          mode="normal"
+          src="https://assets5.lottiefiles.com/packages/lf20_fihetjef.json"
+        >
+        </lottie-player>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/home.scss";
+</style>
--- src/views/who.vue
+++ src/views/who.vue
@@ -1,4 +1,22 @@
 <template>
-	<div class="who">Who</div>
+  <div class="dashboard">
+    <Navbar />
+    <Who />
+    <Footer />
+  </div>
 </template>
-
+<script>
+import Navbar from "../components/Navbar";
+import Who from "../components/Who";
+import Footer from "../components/Footer";
+export default {
+  beforeCreate: function() {
+    document.body.className = 'home';
+  },
+  components: {
+    Navbar,
+    Who,
+    Footer
+  }
+};
+</script>
